a2fe36be3a6346280bb9bb88240ddedc3ac66d74
FelixMo42 <felix@tacocat.com>
2021-08-11 09:47:47 -0700

    added easy wayland/x11 toggle

--- configuration.nix
+++ configuration.nix
@@ -1,6 +1,9 @@
 { config, pkgs, ... } :
 
 let
+    use_wayland = true;
+
+    # bring in custom packages
     nvim = pkgs.callPackage ./nvim {};
     python = pkgs.callPackage ./python {};
 in {
@@ -17,6 +20,9 @@ in {
 	programs.fish.shellInit = builtins.readFile ./fish/init.fish;
     programs.fish.promptInit = builtins.readFile ./fish/prompt.fish;
 	users.users.felix = { shell = pkgs.fish; };
+    
+    # Adb for android stuff
+    programs.adb.enable = true;
 
 	# Use the systemd-boot EFI boot loader.
 	boot.loader.systemd-boot.enable = true;
@@ -28,6 +34,9 @@ in {
     # enable bluetooth
     hardware.bluetooth.enable = true;
     services.blueman.enable = true;
+
+    # power managment
+    services.upower.enable = true;
 
 	# Enable networking
 	networking.hostName = "felixia"; # Define your hostname.
@@ -53,26 +62,39 @@ in {
 		extraGroups = [
 			"wheel" # let us use sudo
 			"networkmanager" # let us edit network connection
+            "adbusers" # lets use use abd
 		];
 	};
 
-    # Set up config
-    programs.sway.enable = true;
+    
+
+    programs.sway.enable = use_wayland;
     environment.etc = {
-        "sway/config".source = ./sway/config;
+       "sway/config".source = ./sway/config;
     };
 
+    services.xserver.enable = !use_wayland;
+    services.xserver.desktopManager.xfce.enable = !use_wayland;
+
     # Lets play some games
+    nixpkgs.config.packageOverrides = pkgs: {
+        steam = pkgs.steam.override {
+            extraPkgs = pkgs: [
+                pkgs.pipewire
+            ];
+        };
+    };
     programs.steam.enable = true;
 
 	# Install packages
 	environment.systemPackages = with pkgs; [
 		# applications
-		firefox-wayland # browser
+        firefox-wayland # browser
         alacritty       # terminal
         krita           # art tool
         minecraft       # minecraft
         zoom-us         # zoom
+        teams           # microsoft teams
 
         # terminal applications
         nvim    # editor
@@ -86,6 +108,7 @@ in {
         ripgrep       # better grep
         brightnessctl # adjust screen brightness
         zip unzip     # zipping utilitys
+        steamcmd      # steam tui stuff
 
 		# languages
 		rustup    # rust
@@ -103,6 +126,6 @@ in {
 	];
 
     # Nixos version
-	system.stateVersion = "20.09";
+	system.stateVersion = "21.05";
 }
 
